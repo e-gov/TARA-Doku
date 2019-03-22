6f8d12104a3943949533016b5dd7b593143fa96d
Priit Parmakson <priitp@ria.ee>
2019-03-22 02:02:05 +0200

    Valgustusrežiim lisatud

--- js/Utils.js
+++ js/Utils.js
@@ -5,8 +5,20 @@ function init() {
   // $('.alammenyy').hide();
 
   // Bind toggling handler to submenu switches
-  $('.lyliti').click(function() {
+  /* $('.lyliti').click(function() {
     var i = $(this).attr('id');
     $('#alammenyy' + i).toggle();
-  });
+  }); */
+
+  // Sea valgustusrežiimi lüliti
+  $('#paike').click(() => {
+    $('#paike').addClass('hidden');
+    $('#kuu').removeClass('hidden');
+    document.documentElement.setAttribute('data-theme', 'light');
+  }); 
+  $('#kuu').click(() => {
+    $('#kuu').addClass('hidden');
+    $('#paike').removeClass('hidden');
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }); 
 }
