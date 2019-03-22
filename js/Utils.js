896fa81a0468b4b233d4f41929f13a08cbb3059f
Priit Parmakson <priitp@ria.ee>
2019-03-22 02:34:41 +0200

    +

--- js/Utils.js
+++ js/Utils.js
@@ -10,15 +10,24 @@ function init() {
     $('#alammenyy' + i).toggle();
   }); */
 
+  // Kui valgusrežiim on valitud, siis rakenda see
+  var valgus = localStorage.getItem('valgus');
+  if (valgus) {
+    document.documentElement.setAttribute('data-theme', valgus);
+  }
+
   // Sea valgustusrežiimi lüliti
   $('#paike').click(() => {
     $('#paike').addClass('hidden');
     $('#kuu').removeClass('hidden');
     document.documentElement.setAttribute('data-theme', 'light');
+    // Salvesta valik lokaalmällu
+    localStorage.setItem('valgus', 'light');
   }); 
   $('#kuu').click(() => {
     $('#kuu').addClass('hidden');
     $('#paike').removeClass('hidden');
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('valgus', 'dark');
   }); 
 }
